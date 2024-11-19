9f8ebd0c16578d9d72011edf951ddbb946d730cc
VitorMartinsSilva <vtrmartins42@gmail.com>
2024-11-19 22:58:31 +0000

    Nivel Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,27 +2,34 @@
 
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
 
 int main() 
 {
-    int  MatrizNaval [3] [3] = 
+    int  MatrizNaval [10] [10] = 
     {
-        {3, 3, 0},      // O número '3' representa onde estão as partes dos navios
-        {0, 0, 3},
-        {0, 0, 3}
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 3},      // O número '3' representa onde estão as partes dos navios
+        {0, 0, 0, 3, 0, 0, 0, 0, 3, 0},
+        {0, 0, 0, 3, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 3, 0, 0, 0},
+        {0, 0, 0, 0, 0, 3, 0, 0, 0, 0},
+        {3, 3, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
     };
+ 
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf(" %d ", MatrizNaval [i] [j]); // Imprime a matriz
+        }
+        
+            printf("\n"); // Pular para próxima linha da matriz
 
-   // Exibindo onde estão cada parte de cada navio  
-   printf("Parte 1 do navio 1 posicionado na linha [0] coluna [0] %d\n", MatrizNaval [0] [0]);
-   printf("Parte 2 do navio 1 posicionado na linha [0] coluna [1] %d\n", MatrizNaval [0] [1]);
-   printf("Parte 1 do navio 2 posicionado na linha [1] coluna [2] %d\n", MatrizNaval [1] [2]);
-   printf("Parte 1 do navio 1 posicionado na linha [2] coluna [2] %d\n", MatrizNaval [2] [2]);
+    }
     
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
